f613ac9ba82f5c70f2b7f1aa6ee351328db8a4ca
Martin Guy <martinwguy@gmail.com>
2016-07-25 20:19:12 +0200

    Add the rest of the images from the thesis
    We weer only doing the most interesting ones. This adds the others.

--- anal/run.sh
+++ anal/run.sh
@@ -15,52 +15,58 @@ set -e		# Exit if anything fails unexpectedly
 groffset=91
 
 (
+    echo -n "-73 -13 5 651 28 91 2.25 "
+    echo "Fig 0.1 Music of the Brisbane School 2.25s"
+
     echo -n "-63 -4 5 949 27 91 11 "
-    echo "Fig II.4 CDD-1-7-37 2'49\"-3'00\" Water makeup.png"
+    echo "Fig II.4 CDD-1-7-37 2'49\"-3'00\" Water makeup"
+
+    echo -n "-65 -13 257 5882 23 91 2.4 "
+    echo "Fig III.2 CDD-1-7-67 4'53.2\"-4'55.6\" Way Out makeup type A - rhythm pattern"
 
     echo -n "-58 -9 23 515 15 91 2.4 "
-    echo "Fig III.3 CDD-1-7-67 4'53.2\"-4'55.6\" Pot au Feu makeup type B.png"
+    echo "Fig III.3 CDD-1-7-67 4'53.2\"-4'55.6\" Way Out makeup type B - bass pattern"
 
     echo -n "-69 -9 70 18562 28 97 9 "
-    echo "Fig III.4 CDD-1-7-67 7'59\".8'08\" Pot Au Feu makeup Type D.png"
+    echo "Fig III.4 CDD-1-7-67 7'59\"-8'08\" Way Out makeup type D - upward-trending melody"
 
     echo -n "-71 -11 23 3914 30 91 12 "
-    echo "Fig III.5 CDD-1-7-67 10'39\"-10'51\" Pot Au Feu makeup Type F.png"
+    echo "Fig III.5 CDD-1-7-67 10'39\"-10'51\" Way Out makeup type F - sinewave oscillator"
 
     echo -n "-67 -9 46 3515 20 91 100 "
-    echo "Fig III.7 CDD-1-7-68 1'21\"-2'51\" Pot Au Feu early version.png"
+    echo "Fig III.7 CDD-1-7-68 1'21\"-2'51\" Pot Au Feu early version"
+
+    echo -n "-73 -13 16 6809 30 91 60 "
+    echo "Fig IV.2 Putative synthesis using DD334 calculated partials"
 
     echo -n "-56 -7 11 6808 30 91 240 "
-    echo "Fig IV.3 CDD-1-3-5 0'32\"-4'32\" Lowell.png"
+    echo "Fig IV.3 CDD-1-3-5 0'32\"-4'32\" Lowell"
 
     echo -n "-71 -12 11 6972 19 91 150 "
-    echo "Fig IV.4 CDD-1-6-3 15'54\"-18'26\" Random Together I.png"
-
-    #echo -n "-73 -13 16 6809 30 91 60 "
-    #echo "test-image.png"
+    echo "Fig IV.4 CDD-1-6-3 15'54\"-18'26\" Random Together I"
 
     #echo -n "-73 -0 16 6809 30 91 10 "
-    #echo "test.png"
+    #echo "test"
 
-) | while read dbmin dbmax fmin fmax fmaxat groffset duration filename
+) | while read dbmin dbmax fmin fmax fmaxat groffset duration filestem
 do
-    echo $filename
-    audiofile="`echo "$filename" | sed -n 's/\.png$/.wav/p'`"
-    test "$audiofile" || exit 1
+    echo $filestem
+    imagefile="$filestem".png
+    audiofile="$filestem".wav
 
     # Measure the total size of the graphic in pixels.
     # width includes the width of the legend area on the left while the
     # height of the image is the same as the height of the graph data.
-    width=`identify "$filename" | sed -n 's/.* \([1-9][0-9]*\)x\([1-9][0-9]*\) .*/\1/p'`
+    width=`identify "$imagefile" | sed -n 's/.* \([1-9][0-9]*\)x\([1-9][0-9]*\) .*/\1/p'`
     test "$width" || exit 1
-    height=`identify "$filename" | sed -n 's/.* \([1-9][0-9]*\)x\([1-9][0-9]*\) .*/\2/p'`
+    height=`identify "$imagefile" | sed -n 's/.* \([1-9][0-9]*\)x\([1-9][0-9]*\) .*/\2/p'`
     test "$height" || exit 1
 
     # Extract frequency scale
-    convert "$filename" -crop 1x`expr $height - 39 - 20`+32+39 scale$$.png
+    convert "$imagefile" -crop 1x`expr $height - 39 - 20`+32+39 scale$$.png
 
     # Extract raw spectrogram
-    convert "$filename" -crop `expr $width - $groffset`x$height+${groffset}+0 graph$$.png
+    convert "$imagefile" -crop `expr $width - $groffset`x$height+${groffset}+0 graph$$.png
 
     # Figure out the frequency represented by the top row of pixels.
     # In the Percival spectrograms, the frequency represented by the bottom row
